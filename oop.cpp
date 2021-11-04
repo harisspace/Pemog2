d267772d5df27bad01eaa67d61813e9366b01398
Haris Akbar <68624458+harisspace@users.noreply.github.com>
2021-11-04 20:53:12 +0700

    fix: split class with header file

--- oop.cpp
+++ oop.cpp
@@ -3,48 +3,10 @@
 
 using namespace std;
 
-class Vehicle {
-    private:
-        string kind;
-        string model;
-        string name;
-        int year;
-    public:
-        Vehicle(string kind) {
-            this->kind = kind;
-        };
-        
-        string getModel() {
-            return this->model;
-        }
-        
-        void setModel(string model) {
-            this->model = model;
-        }
-        
-        string getName() {
-            return this->name;
-        }
-        
-        void setName(string name) {
-            this->name = name;
-        }
-        
-        int getYear() {
-            return this->year;
-        }
-        
-        void setYear(int year) {
-            this->year = year;
-        }
-        
-        void displayAll() {
-            cout << "Jenis kendaraan: " << this->kind << endl;
-            cout << "Model kendaraan: " << this->model << endl;
-            cout << "Nama kendaraan: " << this->name << endl;
-            cout << "Tahun kendaraan realese: " << this->year << endl;
-        };
-};
+#include <iostream>
+#include <class.h>
+
+using namespace std;
 
 int main() {
     Vehicle* vehicle = new Vehicle("Car");
